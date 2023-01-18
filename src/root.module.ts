b528387ac72619db7043de07faa0f2efa5b94cc8
asishmindfire <mfsi.asish.s@gmail.com>
2023-01-18 17:36:25 +0530

    Add rate limiter globally with 429 too many requests

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ParkingModule } from './parking/parking.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }),
-    ParkingModule
+    ParkingModule,
+  ThrottlerModule.forRoot({
+    ttl: 1,     // sec
+    limit: 429,
+  })
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class RootModule {
   constructor() {
